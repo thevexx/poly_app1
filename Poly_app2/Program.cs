cb8d27f1982d132219c4ed4be298905756f0d907
chehir vexx <chehir vexx>
2017-03-05 13:21:05 +0100

    2nd commit

--- Poly_app2/Program.cs
+++ Poly_app2/Program.cs
@@ -10,24 +10,29 @@ namespace Poly_app2
     {
         static void Main(string[] args)
         {
+            // afficher un message dans la fenetre console
             Console.Write("HELLO WORLD6346");
 
+            // declaration d'une variable a de type entier et l'initialiser a 5
             int a = 5;
 
-            float b = 10;
+            // declaration d'une variable b de type reel et l'initialiser a 10.2
+            float b = 10.2F;
 
+            // declaration d'une variable c de type booleen et l'initialiser a vrai
             bool c = true;
 
+            // affectation a la variable c une valeur faux
             c = false;
 
+            // declaration d'une variable d de type caractere et l'initialiser a 'a'
             char d = 'a';
-
             
-
             a = 10;
 
             a = a + a;
 
+            // l'execution de la fenetre console s'interempu en attendent l'utilisateur de saisir.
             Console.ReadKey();
         }
     }
